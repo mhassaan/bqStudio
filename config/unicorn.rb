ad7ce17c577b7bf073b91a27e2225e5b43fb89e5
Your Name <you@example.com>
2015-08-16 19:25:00 -0400

    unicorn nginx add ed

---
+++ config/unicorn.rb
@@ -0,0 +1,21 @@
+# set path to application
+app_dir = "/root/bqStudio"
+shared_dir = "#{app_dir}/shared"
+working_directory app_dir
+
+
+# Set unicorn options
+worker_processes 2
+preload_app true
+timeout 30
+
+# Set up socket location
+#listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
+listen "/tmp/unicorn.shop.sock"
+
+# Logging
+stderr_path "#{shared_dir}/log/unicorn.stderr.log"
+stdout_path "#{shared_dir}/log/unicorn.stdout.log"
+
+# Set master PID location
+pid "/var/tmp/unicorn.pid"
