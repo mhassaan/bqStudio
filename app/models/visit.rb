edd6beaab45fe3f9dea564de98d2f4e0a0871655
Your Name <mhassaanchaudhry@gmail.com>
2015-09-04 19:00:39 +0500

    footer chagnes

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -3,7 +3,4 @@ class Visit < ActiveRecord::Base
 	has_attached_file :cover_pic, :styles => { :natural=>"550x367>",:medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :cover_pic, :content_type => /\Aimage\/.*\Z/
 
-	def to_param
-    name
-  end
 end
