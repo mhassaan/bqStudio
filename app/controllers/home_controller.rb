22d900857467b65d60bf1a261553e2d7dff3fe90
Your Name <mhassaanchaudhry@gmail.com>
2015-09-08 01:58:44 +0500

    css changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
 	def index
-		@data = Visit.paginate(:per_page => 6, :page => params[:page]).order('created_at DESC')		
+		@data = Artist.paginate(:per_page => 6, :page => params[:page]).order('created_at DESC')		
 		@banner = Banner.all.order('created_at DESC').limit(3) 
 		
 	end
